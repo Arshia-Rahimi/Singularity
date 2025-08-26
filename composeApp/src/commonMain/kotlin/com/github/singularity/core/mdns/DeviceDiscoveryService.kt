d4484971b0f9dd256f30b3a0e26532ad1dde96ef
Arshia-Rahimi <arshiarahimi.ar81@gmail.com>
2025-08-26 12:56:50 +0330

    create method to discover joined server

--- composeApp/src/commonMain/kotlin/com/github/singularity/core/mdns/DeviceDiscoveryService.kt
+++ composeApp/src/commonMain/kotlin/com/github/singularity/core/mdns/DeviceDiscoveryService.kt
@@ -1,9 +1,13 @@
 package com.github.singularity.core.mdns
 
+import com.github.singularity.core.common.util.Resource
+import com.github.singularity.core.database.entities.JoinedSyncGroup
 import kotlinx.coroutines.flow.Flow
 
 interface DeviceDiscoveryService {
 
     fun discoverServers(): Flow<List<Server>>
+
+    fun discoverServer(syncGroup: JoinedSyncGroup): Flow<Resource<Server>>
     
 }
--- composeApp/src/commonMain/kotlin/com/github/singularity/core/mdns/impl/MdnsDeviceDiscoveryService.kt
+++ composeApp/src/commonMain/kotlin/com/github/singularity/core/mdns/impl/MdnsDeviceDiscoveryService.kt
@@ -2,11 +2,16 @@ package com.github.singularity.core.mdns.impl
 
 import com.appstractive.dnssd.DiscoveryEvent
 import com.appstractive.dnssd.discoverServices
+import com.github.singularity.core.common.util.asResult
+import com.github.singularity.core.database.entities.JoinedSyncGroup
 import com.github.singularity.core.mdns.DeviceDiscoveryService
 import com.github.singularity.core.mdns.MDNS_SERVICE_TYPE
 import com.github.singularity.core.mdns.Server
 import com.github.singularity.core.mdns.toServer
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.runningFold
 
@@ -26,5 +31,20 @@ class MdnsDeviceDiscoveryService : DeviceDiscoveryService {
         }
         .map { it.toList() }
         .distinctUntilChanged()
+
+    override fun discoverServer(syncGroup: JoinedSyncGroup) = flow {
+        discoverServices(MDNS_SERVICE_TYPE).collect { newServer ->
+            when (newServer) {
+                is DiscoveryEvent.Discovered -> newServer.resolve()
+                is DiscoveryEvent.Resolved -> {
+                    val server = newServer.service.toServer()
+                    if(server.syncGroupId == syncGroup.joinedSyncGroupId) {
+                        emit(server)
+                    }
+                }
+                else -> Unit
+            }
+        }
+    }.asResult(Dispatchers.IO)
 
 }
