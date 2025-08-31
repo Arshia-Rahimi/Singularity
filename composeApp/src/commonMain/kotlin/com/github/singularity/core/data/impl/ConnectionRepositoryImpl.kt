ebe6ec5b8e8d247f3ebd654cb256d4fad6b43ef0
Arshia-Rahimi <arshiarahimi.ar81@gmail.com>
2025-08-31 14:56:02 +0330

    incomplete fix

--- composeApp/src/commonMain/kotlin/com/github/singularity/core/data/impl/ConnectionRepositoryImpl.kt
+++ composeApp/src/commonMain/kotlin/com/github/singularity/core/data/impl/ConnectionRepositoryImpl.kt
@@ -5,30 +5,31 @@ import com.github.singularity.core.database.LocalJoinedSyncGroupsDataSource
 import com.github.singularity.core.mdns.DeviceDiscoveryService
 import com.github.singularity.core.shared.model.ConnectionState
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.shareIn
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class ConnectionRepositoryImpl(
     joinedSyncGroupsDataSource: LocalJoinedSyncGroupsDataSource,
-    private val deviceDiscoveryService: DeviceDiscoveryService,
     scope: CoroutineScope,
+    deviceDiscoveryService: DeviceDiscoveryService,
 ) : ConnectionRepository {
 
     override val connection = joinedSyncGroupsDataSource.joinedSyncGroups
         .map { it.firstOrNull { group -> group.isDefault } }
-        .map { defaultServer ->
-            if (defaultServer == null) {
-                ConnectionState.NoDefaultServer
+        .flatMapLatest { defaultServer ->
+            if (defaultServer == null) return@flatMapLatest flow { emit(null) }
+            ConnectionState.Searching(defaultServer)
+            val server = deviceDiscoveryService.discoverServer(defaultServer)
+            if (server == null) {
+                ConnectionState.ServerNotFound(defaultServer, "time out")
             } else {
-                ConnectionState.Searching(defaultServer)
-                val server = deviceDiscoveryService.discoverServer(defaultServer)
-                if (server == null) {
-                    ConnectionState.ServerNotFound(defaultServer, "time out")
-                } else {
-                    // todo connect websocket
-                    ConnectionState.Connected(server)
-                }
+                // todo connect websocket
+                ConnectionState.Connected(server)
             }
         }.shareIn(scope, SharingStarted.WhileSubscribed(5000), 1)
 
