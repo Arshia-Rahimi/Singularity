a4f413167042efd6742b36e726a208bdf4b1479e
Arshia-Rahimi <arshia009k@proton.me>
2025-09-06 13:22:29 +0330

    remove PluginEventsRepository

--- composeApp/src/commonMain/kotlin/com/github/singularity/core/data/di/DataModule.kt
+++ composeApp/src/commonMain/kotlin/com/github/singularity/core/data/di/DataModule.kt
@@ -2,12 +2,10 @@ package com.github.singularity.core.data.di
 
 import com.github.singularity.core.data.ClientConnectionRepository
 import com.github.singularity.core.data.DiscoverRepository
-import com.github.singularity.core.data.PluginEventsRepository
 import com.github.singularity.core.data.PreferencesRepository
 import com.github.singularity.core.data.impl.ClientConnectionRepositoryImpl
 import com.github.singularity.core.data.impl.DiscoverRepositoryImp
 import com.github.singularity.core.data.impl.OfflinePreferencesRepository
-import com.github.singularity.core.data.impl.PluginEventsRepositoryImpl
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
@@ -16,5 +14,4 @@ val DataModule = module {
     singleOf(::ClientConnectionRepositoryImpl) bind ClientConnectionRepository::class
     singleOf(::OfflinePreferencesRepository) bind PreferencesRepository::class
     singleOf(::DiscoverRepositoryImp) bind DiscoverRepository::class
-    singleOf(::PluginEventsRepositoryImpl) bind PluginEventsRepository::class
 }
