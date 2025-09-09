b97e45bb7ccfd7b4e70e542e61117701f56b2619
Arshia-Rahimi <arshia009k@proton.me>
2025-09-09 14:09:12 +0330

    create pairRequest ui in DiscoverScreen.kt

--- composeApp/src/commonMain/kotlin/com/github/singularity/ui/feature/discover/DiscoverScreen.kt
+++ composeApp/src/commonMain/kotlin/com/github/singularity/ui/feature/discover/DiscoverScreen.kt
@@ -1,9 +1,15 @@
 package com.github.singularity.ui.feature.discover
 
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -16,11 +22,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.github.singularity.core.shared.compose.getString
+import com.github.singularity.ui.feature.discover.components.PairRequestState
 import com.github.singularity.ui.feature.discover.components.ServerItem
 import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.viewmodel.koinViewModel
 import singularity.composeapp.generated.resources.Res
 import singularity.composeapp.generated.resources.arrow_back
+import singularity.composeapp.generated.resources.await_pair_request_approval
 import singularity.composeapp.generated.resources.back
 import singularity.composeapp.generated.resources.discover
 
@@ -66,16 +74,43 @@ private fun DiscoverScreen(
             )
         },
     ) { ip ->
-        LazyColumn(
+        Column(
             modifier = Modifier.fillMaxSize()
                 .padding(ip)
                 .padding(4.dp),
         ) {
-            items(uiState.servers) {
-                ServerItem(
-                    server = it,
-                    execute = execute,
-                )
+            AnimatedContent(
+                targetState = uiState.pairRequestState,
+            ) {
+                if (it !is PairRequestState.Idle) {
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                            .padding(vertical = 8.dp),
+                        horizontalArrangement = Arrangement.Center,
+                    ) {
+                        when (uiState.pairRequestState) {
+                            is PairRequestState.Awaiting -> {
+                                Text(Res.string.await_pair_request_approval.toString())
+                                CircularProgressIndicator()
+                            }
+
+                            is PairRequestState.Success -> Text(Res.string.await_pair_request_approval.toString())
+                            is PairRequestState.Error -> Text(uiState.pairRequestState.message)
+                            is PairRequestState.Idle -> Unit
+                        }
+                    }
+                }
+            }
+            LazyColumn(
+                modifier = Modifier.weight(1f)
+                    .fillMaxWidth(),
+            ) {
+                items(uiState.servers) {
+                    ServerItem(
+                        server = it,
+                        execute = execute,
+                    )
+                }
             }
         }
     }
