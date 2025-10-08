f662151fa20ccfeea662a8c8c88af03d2a6c9da1
Arshia-r0 <arshia009k@proton.me>
2025-10-08 12:02:29 +0330

    set minimum window size

--- composeApp/src/desktopMain/kotlin/com/github/singularity/app/Main.kt
+++ composeApp/src/desktopMain/kotlin/com/github/singularity/app/Main.kt
@@ -1,12 +1,18 @@
 package com.github.singularity.app
 
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import java.awt.Dimension
 
 fun main() = application {
     Window(
         onCloseRequest = ::exitApplication,
+        undecorated = false,
+        state = rememberWindowState(width = 800.dp, height = 600.dp)
     ) {
+        window.minimumSize = Dimension(500, 350)
         App()
     }
 }
