9a28d7179307ddbb02e06ca3f14985861914cfad
arshia rahimi <arshia@roseoj.com>
2025-10-12 15:53:33 +0800

    add more scaling options

--- composeApp/src/commonMain/kotlin/com/github/singularity/core/shared/Enums.kt
+++ composeApp/src/commonMain/kotlin/com/github/singularity/core/shared/Enums.kt
@@ -26,25 +26,49 @@ enum class ScaleOption(
 		value = 0.5f,
 		label = "50%",
 	),
+	S62_5(
+		value = 0.625f,
+		label = "62.5%",
+	),
 	S75(
 		value = 0.75f,
 		label = "75%",
+	),
+	S87_5(
+		value = 0.875f,
+		label = "87.5%",
 	),
 	S100(
 		value = 1f,
 		label = "100%",
 	),
+	S112_5(
+		value = 1.125f,
+		label = "112.5%",
+	),
 	S125(
-		value = 1.25f,
+		value = 1.125f,
 		label = "125%",
+	),
+	S137_5(
+		value = 1.375f,
+		label = "137.5%",
 	),
 	S150(
 		value = 1.5f,
 		label = "150%",
 	),
+	S162_5(
+		value = 1.625f,
+		label = "162.5%",
+	),
 	S175(
 		value = 1.75f,
 		label = "175%",
+	),
+	S187_5(
+		value = 1.875f,
+		label = "187.5%",
 	),
 	S200(
 		value = 2f,
