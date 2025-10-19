444e046f1ff64473f31eedefc835b2381fc86633
arshia rahimi <arshia@roseoj.com>
2025-10-19 12:06:41 +0800

    fix broadcast on macos

--- composeApp/src/commonMain/kotlin/com/github/singularity/core/broadcast/Utils.kt
+++ composeApp/src/commonMain/kotlin/com/github/singularity/core/broadcast/Utils.kt
@@ -2,9 +2,8 @@ package com.github.singularity.core.broadcast
 
 import com.github.singularity.core.shared.deviceName
 import com.github.singularity.core.shared.model.HostedSyncGroup
-import com.github.singularity.core.shared.platform
 
 const val MDNS_SERVICE_TYPE = "_singularity._tcp"
 
 fun getServiceName(group: HostedSyncGroup) =
-    "${group.name}@Singularity-$platform-${deviceName}"
+    "${group.name}@Singularity-${deviceName}"
--- composeApp/src/desktopMain/kotlin/com/github/singularity/core/shared/Utils.desktop.kt
+++ composeApp/src/desktopMain/kotlin/com/github/singularity/core/shared/Utils.desktop.kt
@@ -5,6 +5,8 @@ import java.net.InetAddress
 actual val deviceName: String
     get() = try {
         InetAddress.getLocalHost().hostName
+            // remove .local suffix on macos
+            .removeSuffix(".local")
     } catch (e: Exception) {
         "Unknown Device"
     }
