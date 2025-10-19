322b832a0a3bcf2fa16316085ccaf0d4284baa3c
arshia rahimi <arshia@roseoj.com>
2025-10-19 16:02:16 +0800

    update settings ui

--- composeApp/src/commonMain/kotlin/com/github/singularity/ui/feature/settings/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/github/singularity/ui/feature/settings/SettingsScreen.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -24,10 +27,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.github.singularity.core.shared.ScaleOption
 import com.github.singularity.core.shared.compose.getPainter
 import com.github.singularity.core.shared.compose.getString
+import com.github.singularity.core.shared.compose.onCondition
 import com.github.singularity.core.shared.compose.select
 import com.github.singularity.ui.designsystem.components.DrawerIcon
 import com.github.singularity.ui.designsystem.components.ScreenScaffold
@@ -67,67 +72,85 @@ private fun SettingsScreen(
         LazyColumn(
             modifier = Modifier.fillMaxSize()
                 .padding(ip),
-            verticalArrangement = Arrangement.spacedBy(4.dp),
         ) {
-            item {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable { SettingsIntent.ToggleTheme.execute() }
-                        .padding(horizontal = 8.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    Text("theme: ${uiState.theme.title.getString()}")
-                }
+            settingsItem(
+                onClick = { SettingsIntent.ToggleTheme.execute() },
+            ) {
+                SettingsItemText("Theme: ")
+                SettingsItemText(uiState.theme.title.getString())
             }
 
-            item {
+            settingsItem {
                 var expanded by remember { mutableStateOf(false) }
-                Row(
-                    modifier = Modifier.fillMaxWidth()
-                        .padding(horizontal = 8.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically,
-                ) {
-                    Text("scale: ")
-                    Box {
-                        OutlinedTextField(
-                            enabled = false,
-                            readOnly = true,
-                            value = uiState.scale.label,
-                            onValueChange = {},
-                            trailingIcon = {
-                                IconButton(
-                                    onClick = { expanded = true },
-                                ) {
-                                    Icon(
-                                        painter = expanded.select(
-                                            Res.drawable.arrow_down,
-                                            Res.drawable.arrow_up,
-                                        ).getPainter(),
-                                        contentDescription = "",
-                                    )
-                                }
-                            },
-                        )
-
-                        DropdownMenu(
-                            expanded = expanded,
-                            onDismissRequest = { expanded = false },
-                        ) {
-                            ScaleOption.entries.forEach {
-                                DropdownMenuItem(
-                                    text = { Text(it.label) },
-                                    onClick = {
-                                        SettingsIntent.ChangeScale(it).execute()
-                                        expanded = false
-                                    }
+                SettingsItemText("Scale:")
+                Box {
+                    OutlinedTextField(
+                        enabled = false,
+                        readOnly = true,
+                        value = uiState.scale.label,
+                        onValueChange = {},
+                        trailingIcon = {
+                            IconButton(
+                                onClick = { expanded = true },
+                            ) {
+                                Icon(
+                                    painter = expanded.select(
+                                        Res.drawable.arrow_down,
+                                        Res.drawable.arrow_up,
+                                    ).getPainter(),
+                                    contentDescription = "",
                                 )
                             }
+                        },
+                    )
+
+                    DropdownMenu(
+                        expanded = expanded,
+                        onDismissRequest = { expanded = false },
+                    ) {
+                        ScaleOption.entries.forEach {
+                            DropdownMenuItem(
+                                text = { Text(it.label) },
+                                onClick = {
+                                    SettingsIntent.ChangeScale(it).execute()
+                                    expanded = false
+                                }
+                            )
                         }
                     }
                 }
             }
         }
     }
+}
+
+private fun LazyListScope.settingsItem(
+    onClick: (() -> Unit)? = null,
+    content: @Composable RowScope.() -> Unit,
+) {
+    item {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp)
+                .onCondition(onClick != null) {
+                    clickable { onClick?.invoke() }
+                }
+                .padding(horizontal = 20.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+        ) {
+            content()
+        }
+    }
+}
+
+@Composable
+private fun SettingsItemText(
+    text: String,
+) {
+    Text(
+        text = text,
+        fontSize = 16.sp,
+    )
 }
