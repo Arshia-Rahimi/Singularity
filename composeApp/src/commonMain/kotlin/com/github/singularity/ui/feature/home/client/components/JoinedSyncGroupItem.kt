60dcc043861f848cbc91e3efdc8e138954b06555
Arshia-Rahimi <arshiarahimi.ar81@gmail.com>
2025-10-26 09:55:23 +0330

    create JoinedSyncGroupItem.kt

--- composeApp/src/commonMain/kotlin/com/github/singularity/ui/feature/home/client/components/JoinedSyncGroupItem.kt
+++ composeApp/src/commonMain/kotlin/com/github/singularity/ui/feature/home/client/components/JoinedSyncGroupItem.kt
@@ -1,16 +1,9 @@
 package com.github.singularity.ui.feature.home.client.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyItemScope
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,13 +11,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.github.singularity.core.shared.compose.getPainter
 import com.github.singularity.core.shared.compose.getString
-import com.github.singularity.core.shared.compose.onCondition
 import com.github.singularity.core.shared.model.JoinedSyncGroup
 import com.github.singularity.ui.designsystem.components.dialogs.ConfirmationDialog
 import com.github.singularity.ui.feature.home.client.ClientIntent
@@ -32,80 +24,43 @@ import singularity.composeapp.generated.resources.Res
 import singularity.composeapp.generated.resources.confirm_action
 import singularity.composeapp.generated.resources.delete
 import singularity.composeapp.generated.resources.delete_group
-import singularity.composeapp.generated.resources.options
 import singularity.composeapp.generated.resources.set_group_as_default
 
 @Composable
-fun LazyItemScope.JoinedSyncGroupItem(
-    joinedSyncGroup: JoinedSyncGroup,
-    modifier: Modifier = Modifier,
-    execute: ClientIntent.() -> Unit,
+fun JoinedSyncGroupItem(
+	joinedSyncGroup: JoinedSyncGroup,
+	execute: ClientIntent.() -> Unit,
 ) {
-    var showSetAsDefaultDialog by remember { mutableStateOf(false) }
-    var showDropDownMenu by remember { mutableStateOf(false) }
-    var showDeletionDialog by remember { mutableStateOf(false) }
+	var showSetAsDefaultDialog by remember { mutableStateOf(false) }
+	var showDeletionDialog by remember { mutableStateOf(false) }
 
-    Row(
-        modifier = modifier.fillMaxWidth()
-            .animateItem()
-            .onCondition(joinedSyncGroup.isDefault) {
-                background(MaterialTheme.colorScheme.secondaryContainer)
-            }
-            .onCondition(!joinedSyncGroup.isDefault) {
-                clickable { showSetAsDefaultDialog = true }
-            }
-            .padding(vertical = 4.dp, horizontal = 8.dp),
-    ) {
-        Text(
-            fontSize = 16.sp,
-            text = joinedSyncGroup.syncGroupName,
-            color = if (joinedSyncGroup.isDefault) MaterialTheme.colorScheme.onSecondaryContainer else Color.Unspecified,
-        )
+	Box(
+		modifier = Modifier
+			.padding(4.dp)
+			.clip(RoundedCornerShape(16.dp))
+			.background(MaterialTheme.colorScheme.secondary)
+			.padding(vertical = 4.dp, horizontal = 8.dp),
+		contentAlignment = Alignment.Center,
+	) {
+		Text(
+			fontSize = 16.sp,
+			text = joinedSyncGroup.syncGroupName,
+		)
+	}
 
-        Box {
-            IconButton(
-                onClick = { showDropDownMenu = true },
-            ) {
-                Icon(
-                    painter = Res.drawable.options.getPainter(),
-                    contentDescription = Res.string.options.getString(),
-                )
-            }
-            DropdownMenu(
-                expanded = showDropDownMenu,
-                onDismissRequest = { showDropDownMenu = false },
-            ) {
-                DropdownMenuItem(
-                    text = { Text(Res.string.delete.getString()) },
-                    onClick = {
-                        showDeletionDialog = true
-                        showDropDownMenu = false
-                    },
-                    trailingIcon = {
-                        Icon(
-                            painter = Res.drawable.delete.getPainter(),
-                            contentDescription = Res.string.delete.getString(),
-                        )
-                    },
-                )
-            }
-        }
+	ConfirmationDialog(
+		visible = showSetAsDefaultDialog,
+		message = Res.string.set_group_as_default.getString(joinedSyncGroup.syncGroupName),
+		onConfirm = { ClientIntent.SetAsDefault(joinedSyncGroup).execute() },
+		onDismiss = { showSetAsDefaultDialog = false },
+	)
 
-    }
-
-    ConfirmationDialog(
-        visible = showSetAsDefaultDialog,
-        message = Res.string.set_group_as_default.getString(joinedSyncGroup.syncGroupName),
-        onConfirm = { ClientIntent.SetAsDefault(joinedSyncGroup).execute() },
-        onDismiss = { showSetAsDefaultDialog = false },
-    )
-
-    ConfirmationDialog(
-        visible = showDeletionDialog,
-        title = Res.string.confirm_action.getString(),
-        message = Res.string.delete_group.getString(joinedSyncGroup.syncGroupName),
-        onConfirm = { ClientIntent.DeleteGroup(joinedSyncGroup).execute() },
-        onDismiss = { showDeletionDialog = false },
-        confirmText = Res.string.delete.getString(),
-    )
+	ConfirmationDialog(
+		visible = showDeletionDialog,
+		title = Res.string.confirm_action.getString(),
+		message = Res.string.delete_group.getString(joinedSyncGroup.syncGroupName),
+		onConfirm = { ClientIntent.DeleteGroup(joinedSyncGroup).execute() },
+		onDismiss = { showDeletionDialog = false },
+		confirmText = Res.string.delete.getString(),
+	)
 }
--- composeApp/src/commonMain/kotlin/com/github/singularity/ui/feature/home/client/pages/DiscoverPage.kt
+++ composeApp/src/commonMain/kotlin/com/github/singularity/ui/feature/home/client/pages/DiscoverPage.kt
@@ -6,20 +6,25 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.github.singularity.core.shared.AppTheme
+import com.github.singularity.core.shared.compose.getString
+import com.github.singularity.core.shared.model.JoinedSyncGroup
 import com.github.singularity.ui.designsystem.components.DrawerIcon
 import com.github.singularity.ui.designsystem.theme.SingularityTheme
 import com.github.singularity.ui.feature.home.client.ClientIntent
 import com.github.singularity.ui.feature.home.client.ClientUiState
+import com.github.singularity.ui.feature.home.client.components.JoinedSyncGroupItem
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import singularity.composeapp.generated.resources.Res
-import singularity.composeapp.generated.resources.hosted_sync_groups
+import singularity.composeapp.generated.resources.joined_sync_groups
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -38,7 +43,7 @@ fun DiscoverPage(
 	}
 
 	LazyVerticalGrid(
-		columns = GridCells.Adaptive(20.dp),
+		columns = GridCells.Adaptive(150.dp),
 		modifier = Modifier.fillMaxSize()
 			.padding(horizontal = 4.dp),
 	) {
@@ -49,8 +54,18 @@ fun DiscoverPage(
 				modifier = Modifier.fillMaxWidth()
 					.padding(vertical = 8.dp, horizontal = 12.dp),
 			) {
-				Text(Res.string.hosted_sync_groups)
+				Text(Res.string.joined_sync_groups.getString())
 			}
+		}
+		items(
+			items = uiState.joinedSyncGroups,
+			key = { it.syncGroupId },
+			contentType = { it },
+		) {
+			JoinedSyncGroupItem(
+				joinedSyncGroup = it,
+				execute = execute,
+			)
 		}
 	}
 }
@@ -61,7 +76,34 @@ private fun Preview() {
 	SingularityTheme(AppTheme.Light) {
 		DiscoverPage(
 			execute = {},
-			uiState = ClientUiState(),
+			uiState = ClientUiState(
+				joinedSyncGroups = listOf(
+					JoinedSyncGroup(
+						authToken = "a",
+						syncGroupId = "1",
+						isDefault = false,
+						syncGroupName = "name",
+					),
+					JoinedSyncGroup(
+						authToken = "a",
+						syncGroupId = "2",
+						isDefault = false,
+						syncGroupName = "name",
+					),
+					JoinedSyncGroup(
+						authToken = "a",
+						syncGroupId = "3",
+						isDefault = false,
+						syncGroupName = "name",
+					),
+					JoinedSyncGroup(
+						authToken = "a",
+						syncGroupId = "4",
+						isDefault = false,
+						syncGroupName = "name",
+					),
+				).toMutableStateList(),
+			),
 			topBar = {},
 		)
 	}
