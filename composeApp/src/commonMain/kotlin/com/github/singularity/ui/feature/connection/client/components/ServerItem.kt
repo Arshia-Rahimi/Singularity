d8adf8b693f3b2921f478944cb396a602189b739
arshia rahimi <arshia@roseoj.com>
2025-11-01 11:34:02 +0800

    show ServerItem.kt details

--- composeApp/src/commonMain/kotlin/com/github/singularity/ui/feature/connection/client/components/ServerItem.kt
+++ composeApp/src/commonMain/kotlin/com/github/singularity/ui/feature/connection/client/components/ServerItem.kt
@@ -2,10 +2,13 @@ package com.github.singularity.ui.feature.connection.client.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.LazyGridItemScope
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.github.singularity.core.shared.compose.getString
 import com.github.singularity.core.shared.model.LocalServer
 import com.github.singularity.ui.designsystem.components.dialogs.ConfirmationDialog
@@ -34,11 +38,26 @@ fun LazyGridItemScope.ServerItem(
             .animateItem()
             .padding(8.dp)
             .clip(RoundedCornerShape(16.dp))
-            .background(MaterialTheme.colorScheme.secondary)
+            .background(MaterialTheme.colorScheme.secondaryContainer)
             .padding(16.dp),
         contentAlignment = Alignment.Center,
     ) {
-
+        Column(
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            Text(
+                text = server.syncGroupName,
+                fontSize = 16.sp,
+            )
+            Text(
+                text = "${server.deviceName} - ${server.deviceOs}",
+                fontSize = 12.sp,
+            )
+            Text(
+                text = server.ip,
+                fontSize = 12.sp,
+            )
+        }
     }
 
     ConfirmationDialog(
