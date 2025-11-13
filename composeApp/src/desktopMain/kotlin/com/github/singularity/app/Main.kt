e22b4c3ab94030a7ded64c221105d4fac414a419
Arshia-Rahimi <arshiarahimi.ar81@gmail.com>
2025-11-13 11:28:44 +0330

    remove initial scaled size enforcement for desktop window

--- composeApp/src/desktopMain/kotlin/com/github/singularity/app/Main.kt
+++ composeApp/src/desktopMain/kotlin/com/github/singularity/app/Main.kt
@@ -1,36 +1,20 @@
 package com.github.singularity.app
 
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
-import org.koin.compose.koinInject
-import org.koin.core.module.dsl.viewModelOf
-import org.koin.dsl.module
 import java.awt.Dimension
 
-private val DesktopModule = module {
-    viewModelOf(::DesktopViewModel)
-}
-
 fun main() = application {
-    initKoin {
-        modules(DesktopModule)
-    }
+	initKoin()
 
-    val viewModel = koinInject<DesktopViewModel>()
-    val scale by viewModel.scale.collectAsState()
+	Window(
+		onCloseRequest = ::exitApplication,
+		state = rememberWindowState(width = 1000.dp, height = 800.dp),
+	) {
+		window.minimumSize = Dimension(500, 350)
 
-    scale?.let { scale ->
-        Window(
-            onCloseRequest = ::exitApplication,
-	        state = rememberWindowState(width = 800.dp * scale, height = 600.dp * scale),
-        ) {
-            window.minimumSize = Dimension(500, 350)
-
-            App()
-        }
-    }
+		App()
+	}
 }
