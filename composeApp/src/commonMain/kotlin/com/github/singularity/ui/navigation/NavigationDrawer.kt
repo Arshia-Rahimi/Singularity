5f926154402cb488c8ff8e559862884c09d7e059
arshia rahimi <arshia@roseoj.com>
2025-11-15 15:43:14 +0800

    udpate NavigationDrawer.kt

--- composeApp/src/commonMain/kotlin/com/github/singularity/ui/navigation/NavigationDrawer.kt
+++ composeApp/src/commonMain/kotlin/com/github/singularity/ui/navigation/NavigationDrawer.kt
@@ -25,7 +25,6 @@ import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,156 +45,155 @@ import singularity.composeapp.generated.resources.singularity
 
 @Composable
 fun NavigationDrawer(
-	navController: NavHostController,
-	uiState: MainUiState,
-	toggleSyncMode: () -> Unit,
-	navHost: @Composable () -> Unit,
+    navController: NavHostController,
+    uiState: MainUiState,
+    toggleSyncMode: () -> Unit,
+    navHost: @Composable () -> Unit,
 ) {
-	val windowSizeClass by rememberWindowSizeClass()
+    val windowSizeClass by rememberWindowSizeClass()
 
-	if (windowSizeClass == WindowSizeClass.Expanded) {
-		PermanentNavigationDrawer(
-			drawerContent = {
-				PermanentDrawerSheet(
-					modifier = Modifier.width(IntrinsicSize.Max),
-				) {
-					DrawerContent(
-						navController = navController,
-						uiState = uiState,
-						toggleSyncMode = toggleSyncMode,
-						closeDrawer = {},
-					)
-				}
-			},
-		) {
-			navHost()
-		}
+    if (windowSizeClass == WindowSizeClass.Expanded) {
+        PermanentNavigationDrawer(
+            drawerContent = {
+                PermanentDrawerSheet(
+                    modifier = Modifier.width(IntrinsicSize.Max),
+                ) {
+                    DrawerContent(
+                        navController = navController,
+                        uiState = uiState,
+                        toggleSyncMode = toggleSyncMode,
+                        closeDrawer = {},
+                    )
+                }
+            },
+        ) {
+            navHost()
+        }
 
-	} else {
+    } else {
 
-		val scope = rememberCoroutineScope()
-		val drawerState = rememberDrawerState(DrawerValue.Closed)
-		val closeDrawer: () -> Unit = { scope.launch { drawerState.close() } }
+        val scope = rememberCoroutineScope()
+        val drawerState = rememberDrawerState(DrawerValue.Closed)
+        val closeDrawer: () -> Unit = { scope.launch { drawerState.close() } }
 
-		ObserveForEvents(AppNavigationController.toggleDrawerEvent) {
-			scope.launch {
-				if (drawerState.isOpen) drawerState.close()
-				else drawerState.open()
-			}
-		}
+        ObserveForEvents(AppNavigationController.toggleDrawerEvent) {
+            scope.launch {
+                if (drawerState.isOpen) drawerState.close()
+                else drawerState.open()
+            }
+        }
 
-		ModalNavigationDrawer(
-			drawerState = drawerState,
-			drawerContent = {
-				ModalDrawerSheet(
-					modifier = Modifier.width(IntrinsicSize.Max),
-				) {
-					DrawerContent(
-						navController = navController,
-						closeDrawer = closeDrawer,
-						uiState = uiState,
-						toggleSyncMode = toggleSyncMode,
-					)
-				}
-			}
-		) {
-			navHost()
-		}
-	}
+        ModalNavigationDrawer(
+            drawerState = drawerState,
+            drawerContent = {
+                ModalDrawerSheet(
+                    modifier = Modifier.width(IntrinsicSize.Max),
+                ) {
+                    DrawerContent(
+                        navController = navController,
+                        closeDrawer = closeDrawer,
+                        uiState = uiState,
+                        toggleSyncMode = toggleSyncMode,
+                    )
+                }
+            }
+        ) {
+            navHost()
+        }
+    }
 
 }
 
 @Composable
 private fun DrawerContent(
-	navController: NavController,
-	uiState: MainUiState,
-	toggleSyncMode: () -> Unit,
-	closeDrawer: () -> Unit,
+    navController: NavController,
+    uiState: MainUiState,
+    toggleSyncMode: () -> Unit,
+    closeDrawer: () -> Unit,
 ) {
-	val windowSizeClass by rememberWindowSizeClass()
-	val currentRoute by navController.currentRoute
+    val windowSizeClass by rememberWindowSizeClass()
+    val currentRoute by navController.currentRoute
 
-	Column(
-		modifier = Modifier
-			.fillMaxSize()
-			.padding(vertical = 12.dp)
-			.padding(horizontal = 8.dp)
-			.verticalScroll(rememberScrollState()),
-		verticalArrangement = Arrangement.SpaceBetween,
-	) {
-		Column(
-			verticalArrangement = Arrangement.spacedBy(4.dp, Alignment.Top),
-			modifier = Modifier.fillMaxWidth(),
-		) {
-			Text(
-				text = Res.string.singularity.getString(),
-				fontSize = 24.sp,
-				color = MaterialTheme.colorScheme.primary,
-				modifier = Modifier.onCondition(windowSizeClass != WindowSizeClass.Expanded) {
-					padding(start = 12.dp)
-				}
-			)
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(vertical = 12.dp)
+            .padding(horizontal = 8.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            Text(
+                text = Res.string.singularity.getString(),
+                fontSize = 24.sp,
+                color = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.onCondition(windowSizeClass != WindowSizeClass.Expanded) {
+                    padding(start = 12.dp)
+                }
+            )
 
-			HorizontalDivider(Modifier.padding(vertical = 4.dp))
+            HorizontalDivider(Modifier.padding(vertical = 4.dp))
 
-			SyncModeSwitch(
-				uiState = uiState,
-				toggleSyncMode = toggleSyncMode,
-			)
+            SyncModeSwitch(
+                uiState = uiState,
+                toggleSyncMode = toggleSyncMode,
+            )
 
-			NavigationDrawerItemTop.entries.forEach { item ->
-				NavigationDrawerItem(
-					item = item,
-					currentRoute = currentRoute,
-					closeDrawer = closeDrawer,
-					navigateTo = { navController.navigate(it) },
-				)
-			}
+            NavigationDrawerItemTop.entries.forEach { item ->
+                NavigationDrawerItem(
+                    item = item,
+                    currentRoute = currentRoute,
+                    closeDrawer = closeDrawer,
+                    navigateTo = { navController.navigate(it) },
+                )
+            }
 
-		}
+        }
 
-		Column(
-			modifier = Modifier.fillMaxWidth(),
-            verticalArrangement = Arrangement.spacedBy(1.dp, Alignment.Top),
-		) {
-			NavigationDrawerItemBottom.entries.forEach { item ->
-				NavigationDrawerItem(
-					item = item,
-					currentRoute = currentRoute,
-					closeDrawer = closeDrawer,
-					navigateTo = { navController.navigate(it) },
-				)
-			}
-		}
-	}
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            NavigationDrawerItemBottom.entries.forEach { item ->
+                NavigationDrawerItem(
+                    item = item,
+                    currentRoute = currentRoute,
+                    closeDrawer = closeDrawer,
+                    navigateTo = { navController.navigate(it) },
+                )
+            }
+        }
+    }
 }
 
 @Composable
 private fun SyncModeSwitch(
-	uiState: MainUiState,
-	toggleSyncMode: () -> Unit,
+    uiState: MainUiState,
+    toggleSyncMode: () -> Unit,
 ) {
-	if (canHostSyncServer) {
-		Row(
-			modifier = Modifier.fillMaxWidth(),
-			horizontalArrangement = Arrangement.Center,
-		) {
-			SingleChoiceSegmentedButtonRow {
-				SyncMode.entries.forEachIndexed { index, mode ->
-					SegmentedButton(
-						selected = uiState.syncMode == mode,
-						label = { Text(text = mode.title.getString()) },
-						onClick = {
-							if (mode == uiState.syncMode) return@SegmentedButton
-							toggleSyncMode()
-						},
-						shape = SegmentedButtonDefaults.itemShape(
-							index = index,
-							count = SyncMode.entries.size,
-						)
-					)
-				}
-			}
-		}
-	}
+    if (canHostSyncServer) {
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(4.dp),
+            horizontalArrangement = Arrangement.Center,
+        ) {
+            SingleChoiceSegmentedButtonRow {
+                SyncMode.entries.forEachIndexed { index, mode ->
+                    SegmentedButton(
+                        selected = uiState.syncMode == mode,
+                        label = { Text(text = mode.title.getString()) },
+                        onClick = {
+                            if (mode == uiState.syncMode) return@SegmentedButton
+                            toggleSyncMode()
+                        },
+                        shape = SegmentedButtonDefaults.itemShape(
+                            index = index,
+                            count = SyncMode.entries.size,
+                        )
+                    )
+                }
+            }
+        }
+    }
 }
