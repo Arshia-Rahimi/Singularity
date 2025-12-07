e165792b14ea3f4d04a1f5f43ac2aa677d778ff1
arshia rahimi <arshia@roseoj.com>
2025-12-07 13:39:32 +0800

    update broadcastReceiver

--- composeApp/src/androidMain/kotlin/com/github/singularity/core/syncservice/BootReceiver.kt
+++ composeApp/src/androidMain/kotlin/com/github/singularity/core/syncservice/BootReceiver.kt
@@ -7,10 +7,9 @@ import android.content.Intent
 class BootReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        if (intent?.action == Intent.ACTION_BOOT_COMPLETED) {
-            val serviceIntent = Intent(context, AndroidApplicationService::class.java)
-            context?.startForegroundService(serviceIntent)
-        }
+        if (intent?.action != Intent.ACTION_BOOT_COMPLETED) return
+        val serviceIntent = Intent(context, AndroidApplicationService::class.java)
+        context?.startForegroundService(serviceIntent)
     }
 
 }
