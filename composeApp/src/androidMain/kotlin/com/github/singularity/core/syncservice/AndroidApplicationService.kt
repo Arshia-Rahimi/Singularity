43bc7f1c0951ae164ba828da13cfbc006c251364
Arshia-Rahimi <arshiarahimi.ar81@gmail.com>
2025-12-24 21:38:12 +0330

    create notification for android service

--- composeApp/src/androidMain/kotlin/com/github/singularity/core/syncservice/AndroidApplicationService.kt
+++ composeApp/src/androidMain/kotlin/com/github/singularity/core/syncservice/AndroidApplicationService.kt
@@ -5,31 +5,34 @@ import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.util.Log
 import androidx.core.app.ActivityCompat
 import org.koin.android.ext.android.inject
 
 class AndroidApplicationService : Service() {
 
-    private val service by inject<SyncService>()
+	private val service by inject<SyncService>()
 
-    override fun onBind(intent: Intent?) = null
+	override fun onBind(intent: Intent?) = null
+
+	override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+		startForeground(1, createNotification())
+		return START_STICKY
+	}
 
 }
 
 fun Context.startAndroidApplicationService() {
-    if (ActivityCompat.checkSelfPermission(
-            this,
-            Manifest.permission.FOREGROUND_SERVICE
-        ) == PackageManager.PERMISSION_GRANTED
-        && ActivityCompat.checkSelfPermission(
-            this,
-            Manifest.permission.FOREGROUND_SERVICE_DATA_SYNC
-        ) == PackageManager.PERMISSION_GRANTED
-    ) {
-        val intent = Intent(this, AndroidApplicationService::class.java)
-        this.startForegroundService(intent)
-    } else {
-        Log.e("MyService", "No permissions!")
-    }
+	if (ActivityCompat.checkSelfPermission(
+			this,
+			Manifest.permission.FOREGROUND_SERVICE
+		) == PackageManager.PERMISSION_GRANTED
+		&& ActivityCompat.checkSelfPermission(
+			this,
+			Manifest.permission.FOREGROUND_SERVICE_DATA_SYNC
+		) == PackageManager.PERMISSION_GRANTED
+	) {
+		createNotificationChannel()
+		val intent = Intent(this, AndroidApplicationService::class.java)
+		this.startForegroundService(intent)
+	}
 }
---
+++ composeApp/src/androidMain/kotlin/com/github/singularity/core/syncservice/ServiceNotification.kt
@@ -0,0 +1,41 @@
+package com.github.singularity.core.syncservice
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import androidx.core.app.NotificationCompat
+import com.github.singularity.app.MainActivity
+
+const val NOTIFICATION_CHANNEL_ID = "android_sync_service"
+
+fun Context.createNotificationChannel() {
+	val channel = NotificationChannel(
+		NOTIFICATION_CHANNEL_ID,
+		"Android SyncService",
+		NotificationManager.IMPORTANCE_LOW,
+	)
+
+	(getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager)
+		.createNotificationChannel(channel)
+}
+
+fun Context.createNotification(): Notification {
+	val intent = Intent(this, MainActivity::class.java)
+	val pendingIntent = PendingIntent.getActivity(
+		this,
+		0,
+		intent,
+		PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE,
+	)
+
+	return NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+		.setContentTitle("Singularity")
+		.setContentText("Sync Service Running...")
+		.setContentIntent(pendingIntent)
+		.setSmallIcon(android.R.drawable.ic_popup_sync)
+		.setOngoing(true)
+		.build()
+}
